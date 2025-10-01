b74f07f68cd9fc491f34a587d443c131d36896ee
Sara <saritamendihla@gmail.com>
2025-09-30 21:46:21 -0600

    funcion para escoger tu herramienta de juego

--- proyecto1corrregido.py
+++ proyecto1corrregido.py
@@ -1,6 +1,13 @@
 import random
-def saludo():
+def saludar_usuario():
     print("bienvenido al juego de piedra, papel o tijera")
     nombre=input(" ¿cual es tu nombre?:") 
     print(f"hola {nombre}, tendras que escoger una de las siguientes opciones  e intentar ganarme. intentalo, tal vez corras con suerte... ")
-saludo()
+saludar_usuario()
+
+def dar_opciones():
+    opciones=["piedra", "papel", "tijera"]
+    print(input(f"tienes que elegir una de las siguientes opciones: {opciones}"))
+dar_opciones()
+
+
