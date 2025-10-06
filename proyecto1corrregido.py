d99b44170a8b47765e435d80f271ad51892c7b87
Sara <saritamendihla@gmail.com>
2025-10-05 22:34:04 -0600

    corregi error en la funcion de elegir 1 o 3 rondas, pero falta hacer que la computadora elija y juegue conmigo

--- proyecto1corrregido.py
+++ proyecto1corrregido.py
@@ -4,23 +4,19 @@ def saludar_usuario():
     nombre=input(" ¿cual es tu nombre?:") 
     print(f"hola {nombre}, tendras que escoger una de las siguientes opciones  e intentar ganarme. intentalo, tal vez corras con suerte... ")
 saludar_usuario()
-def seleccionar_rondas():
+def seleccionar_rundas():
     while True:
-        respuesta = input("¿Quieres jugar 1 o 3 rondas? Escribe el número: ")
-
-        if respuesta == '1' or respuesta == '3':
-            rondas = int(respuesta)
-            return rondas
+        respuesta=input("¿cuantas rondas quieres jugar, 1 o 3? escribe tu respuesta:")
+        
+        if respuesta=='1' or respuesta=='3':
+            resultado=int(respuesta)
+            return resultado
         else:
-            print("Error: Opción inválida. Por favor, escribe solo '1' o '3'.")
-
-
-numero_de_rondas = seleccionar_rondas()
-
-
+            print("debes de elegir solo la opcion 1 o 3")
+seleccionar_rundas()
 
 def dar_opciones():
     opciones=["piedra", "papel", "tijera"]
     print(input(f"tienes que elegir una de las siguientes opciones: {opciones}"))
 dar_opciones()
-#correccion de estas funciones pendiente
+
