db450affe3eae7bafd3553b0430398843905b4d0
Sara <saritamendihla@gmail.com>
2025-10-06 22:25:21 -0600

    comence a hacer la funcion para que la computadora juegue, pero hace falta corregir la logica

--- proyecto1corrregido.py
+++ proyecto1corrregido.py
@@ -2,9 +2,9 @@ import random
 def saludar_usuario():
     print("bienvenido al juego de piedra, papel o tijera")
     nombre=input(" ¿cual es tu nombre?:") 
-    print(f"hola {nombre}, tendras que escoger una de las siguientes opciones  e intentar ganarme. intentalo, tal vez corras con suerte... ")
+    print  (f"hola {nombre}, tendras que escoger una de las siguientes opciones  e intentar ganarme. intentalo, tal vez corras con suerte... ")
 saludar_usuario()
-def seleccionar_rundas():
+def seleccionar_rundas():#repasar
     while True:
         respuesta=input("¿cuantas rondas quieres jugar, 1 o 3? escribe tu respuesta:")
         
@@ -18,5 +18,11 @@ seleccionar_rundas()
 def dar_opciones():
     opciones=["piedra", "papel", "tijera"]
     print(input(f"tienes que elegir una de las siguientes opciones: {opciones}"))
+
+    
+    print(f"la computadora  elegio {eleccion}")
+    eleccion=opciones
+    
+    eleccion=random.choice(opciones)
 dar_opciones()
 
